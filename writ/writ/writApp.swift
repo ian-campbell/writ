319d1a4a13f8e65846dd1ea92ba239852c4dc117
ian-campbell <ian.campbell87@gmail.com>
2020-12-15 23:59:41 -0500

    Add swift files

---
+++ writ/writ/writApp.swift
@@ -0,0 +1,17 @@
+//
+//  writApp.swift
+//  writ
+//
+//  Created by Ian on 12/15/20.
+//
+
+import SwiftUI
+
+@main
+struct writApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
